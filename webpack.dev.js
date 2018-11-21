02509eb54741234f39983e549f98864c771bad5b
Adam Tagscherer <kidama62@gmail.com>
2018-11-21 14:00:00 +0100

    DEV - mini css extract plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,18 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
   devServer: {
     publicPath: '/dist/'
   }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,25 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'pages/[name].bundle.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  }
 });
