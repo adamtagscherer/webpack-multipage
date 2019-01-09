7b11fb06cc81a4b07197fb820ee5df25642f07e8
Adam Tagscherer <kidama62@gmail.com>
2019-01-09 11:14:56 +0100

    FIX - eslinter issues

--- dev/pages/pageA/pageA.js
+++ dev/pages/pageA/pageA.js
@@ -1,6 +1,6 @@
 import '../../common/03_typography.scss';
 import './pageA.scss';
-import '../../blocks/button/button';
+import buttonFunction from '../../blocks/button/button';
 
 const myArray = [1, 2, 3];
 myArray.forEach(element => element * 2);
